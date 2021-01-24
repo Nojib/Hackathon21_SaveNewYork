28c38a72ee99b9c513a2d62e1e27119fd4b1c8b9
Nojib <66505641+Nojib@users.noreply.github.com>
2021-01-24 14:43:51 -0500

    Add files via upload

---
+++ Hackathon/Hackathon21.jsx
@@ -0,0 +1,4 @@
+var yourName = window.prompt("What is your First Name?")
+var Age = window.prompt("How old are you")
+var emailADD = window.prompt("What is your email address")
+document.write("Thanks for registering" + yourName + ",We will send your all the instruction in your " + emailAdd)
